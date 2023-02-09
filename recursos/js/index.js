af1f91f1eb653a959ce76a3beb9f0a80682dd98d
Nefi Avila <avila.nefi1280@gmail.com>
2023-02-08 20:39:37 -0500

    Edición de los archivos CSS y creación de NavBar estable

--- recursos/js/index.js
+++ recursos/js/index.js
@@ -1,3 +1,31 @@
-function myFunction() {
-    document.getElementById('myTopnav').classList.toggle('responsive')
+// NAVBAR RESPONSIVE
+const navToggle = document.querySelector('.tog__navbar');
+const navMenu = document.querySelector('.navbar__menu');
+const navLink = document.querySelectorAll('.link__navbar');
+
+(navToggle).addEventListener('click', () => {
+    navMenu.classList.toggle('navbar--visible');
+})
+
+function linkAction() {
+    navMenu.classList.remove('navbar--visible');
+}
+
+(navLink).forEach(n => n.addEventListener('click', linkAction));
+
+
+// MODO OSCURO
+const input = document.querySelector("input");
+const body = document.querySelector("body");
+
+const toggleThemeMode = () => {
+    body.classList.toggle("dark");
+};
+
+input.onchange = toggleThemeMode;
+
+// ACTUALIZAR AÑO
+function updateYear() {
+    var currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").innerHTML = currentYear;
 }
